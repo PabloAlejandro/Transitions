b90da6a066e514ea2104af18b4806267640dbebc
Pau <pau@depop.com>
2017-02-24 10:28:17 +0000

    Initial Commit

---
+++ Transitions/Transitions.h
@@ -0,0 +1,19 @@
+//
+//  Transitions.h
+//  Transitions
+//
+//  Created by Pau on 24/02/2017.
+//  Copyright © 2017 pau-ios-developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Transitions.
+FOUNDATION_EXPORT double TransitionsVersionNumber;
+
+//! Project version string for Transitions.
+FOUNDATION_EXPORT const unsigned char TransitionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Transitions/PublicHeader.h>
+
+
